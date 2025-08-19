8a671fe4d23b5c27bc8069af58d366257c085cc9
bilalmukhtiar5 <billuedwardian08@gmail.com>
2025-08-19 09:05:51 +0500

    db linked removed

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb+srv://billuedwardian08:adminbilal@cluster0.o9plrh.mongodb.net/");
+    await mongoose.connect("");
     console.log("Connected to MongoDB with Mongoose");
   } catch (err) {
     console.error("MongoDB connection error:", err);
